d2ff248105861795988234c0f57f29501fe3f8b5
HectorSchreiner <agenthector007@gmail.com>
2023-02-17 14:01:16 +0100

    collision done almost
    implementeret collision system til spilleren

--- src/game.rs
+++ src/game.rs
@@ -1,3 +1,4 @@
+use std::array;
 use std::f32::consts::PI;
 
 use crate::Window;
@@ -53,6 +54,74 @@ impl GameRenderer {
         
     }
 
+    // tjekker om player kan bevæge sig og returnerer enten sandt eller falsk
+    pub fn check_player_move(&mut self, direction: i32, player_speed: f32) -> bool {
+        let direction_vec = vec![self.player.direction.sin(), self.player.direction.cos()];
+
+        let mut collides = false;
+
+        let wall_hitbox_arr = initialize_map_hitboxes();
+        // frem
+        if direction == 1 {
+            let future_pos: SquareHitbox = SquareHitbox { 
+                x: self.player.position.x + (player_speed * direction_vec[0]), 
+                y: self.player.position.y + (player_speed * direction_vec[1]), 
+                sideLength: 1.0 
+            };
+            // hvis rammer return true
+            if check_player_collision(&wall_hitbox_arr, &future_pos) {
+                println!("collision");
+                collides = true;
+            }
+        }
+
+        // tilbage
+        if direction == 2 {
+            let future_pos: SquareHitbox = SquareHitbox { 
+                x: self.player.position.x - (player_speed * direction_vec[0]), 
+                y: self.player.position.y - (player_speed * direction_vec[1]), 
+                sideLength: 1.0 
+            };
+            // hvis rammer return false
+            if check_player_collision(&wall_hitbox_arr, &future_pos) {
+                println!("collision");
+
+                collides = true;
+            }
+        }
+
+        // venstre
+        if direction == 3 {
+            let future_pos: SquareHitbox = SquareHitbox { 
+                x: self.player.position.x - (player_speed * direction_vec[1]), 
+                y: self.player.position.y + (player_speed * direction_vec[0]), 
+                sideLength: 1.0 
+            };
+            // hvis rammer return false
+            if check_player_collision(&wall_hitbox_arr, &future_pos) {
+                println!("collision");
+
+                collides = true;
+            }
+        }
+
+        // højre
+        if direction == 4 {
+            let future_pos: SquareHitbox = SquareHitbox { 
+                x: self.player.position.x + (player_speed * direction_vec[1]), 
+                y: self.player.position.y - (player_speed * direction_vec[0]), 
+                sideLength: 1.0 
+            };
+            // hvis rammer return false
+            if check_player_collision(&wall_hitbox_arr, &future_pos) {
+                println!("collision");
+                collides = true;
+            }
+        }
+
+        collides
+    }
+
     pub fn move_player(&mut self, window: &Window) {
         let player_speed = 0.15;
         let rotation_speed = 0.1;
@@ -64,22 +133,32 @@ impl GameRenderer {
             .for_each(|key| match key {
                 //forward backward
                 Key::W => {
+                    if !self.check_player_move(1, player_speed) {
                     self.player.position.x += (player_speed * direction_vec[0]);
                     self.player.position.y += (player_speed * direction_vec[1]);
+                    }
+                    
                 }
                 Key::S => {
+                    if !self.check_player_move(2, player_speed) {
                     self.player.position.x -= (player_speed * direction_vec[0]);
                     self.player.position.y -= (player_speed * direction_vec[1]);
+                    }
                 }
                 //strafe
                 Key::A => {
+                    if !self.check_player_move(3, player_speed) {
                     self.player.position.x -= (player_speed * direction_vec[1]);
                     self.player.position.y += (player_speed * direction_vec[0]);
+                    }
                 }
                 Key::D => {
+                    if !self.check_player_move(4, player_speed) {
                     self.player.position.x += (player_speed * direction_vec[1]);
                     self.player.position.y -= (player_speed * direction_vec[0]);
+                    }
                 }
+
                 //rotate
                 Key::J => {
                     self.player.direction -= rotation_speed;
@@ -185,3 +264,64 @@ pub fn initialize_map() -> Vec<Block> {
 
     map
 }
+
+pub fn initialize_map_hitboxes() -> Vec<SquareHitbox> {
+    let map_array: Vec<Block> = initialize_map();
+    let mut hitbox_array: Vec<SquareHitbox> = vec![];
+
+    fn position(array_index: usize) -> Position2D {
+        let x = (array_index as i32 % MAP_WIDTH_COUNT);
+        let y = (array_index as i32 / MAP_HEIGHT_COUNT);
+
+        Position2D { x: x as f32, y: y as f32 }
+    }
+    
+    for (i, item) in map_array.iter().enumerate() {
+        if *item == Block::Wall {
+            hitbox_array.push(SquareHitbox { x: position(i).x, y: position(i).y, sideLength: 1.0 })
+        }
+    }
+
+    return hitbox_array;
+}
+
+#[derive(Clone, Copy, PartialEq, Debug)]
+pub struct SquareHitbox {
+    x: f32,
+    y: f32,
+    sideLength: f32,
+}
+
+impl SquareHitbox {
+
+    // tjekker om self collider med otherHitbox
+    pub fn self_intersects(&self, otherHitbox: &SquareHitbox) -> bool {
+        let corners = [
+            (self.x, self.y),
+            (self.x + self.sideLength, self.y),
+            (self.x, self.y + self.sideLength),
+            (self.x + self.sideLength, self.y + self.sideLength),
+        ];
+        for &(x, y) in corners.iter() {
+            if x >= otherHitbox.x
+                && x <= otherHitbox.x + otherHitbox.sideLength
+                && y >= otherHitbox.y
+                && y <= otherHitbox.y + otherHitbox.sideLength
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+// tjekker om player collider med en mur i wall array
+pub fn check_player_collision(wallHitboxArr: &[SquareHitbox], playerHitbox: &SquareHitbox) -> bool {
+    for square in wallHitboxArr {
+        if square.self_intersects(playerHitbox) {
+            return true;
+        }
+    }
+    return false;
+}
+
